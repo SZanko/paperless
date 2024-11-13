7604658db5c12577fedef4325f237f33ee562e5d
SZanko <szanko_dev@proton.me>
2024-11-13 21:10:25 +0100

    Implement stupid changes for the test system
    Signed-off-by: SZanko <szanko_dev@proton.me>

--- rest-api/src/main/java/at/fhtw/swkom/paperless/persistence/entities/DocumentModel.java
+++ rest-api/src/main/java/at/fhtw/swkom/paperless/persistence/entities/DocumentModel.java
@@ -1,10 +1,7 @@
 package at.fhtw.swkom.paperless.persistence.entities;
 
 import jakarta.annotation.Nonnull;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.proxy.HibernateProxy;
 
@@ -17,6 +14,7 @@ import java.util.Objects;
 @AllArgsConstructor
 @NoArgsConstructor
 @RequiredArgsConstructor
+@Table(name = "document")
 public class DocumentModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
