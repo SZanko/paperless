599843c448bc6a008bf1806c6145f1ae23f0c23e
SZanko <szanko_dev@proton.me>
2024-12-04 11:53:50 +0100

    Add dependencies for ocr worker
    Signed-off-by: SZanko <szanko_dev@proton.me>

--- ocr-worker/project.clj
+++ ocr-worker/project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [com.novemberain/langohr "5.5.0"]
+                 [clojurewerkz/elastisch "3.0.1"]
+                 [minio-clj "0.2.2"]
+                 [org.clojars.scottxtra/clj-ocr "0.1.0-SNAPSHOT"]]
   :main ^:skip-aot ocr-worker.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
